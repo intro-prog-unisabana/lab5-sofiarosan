52848b72b97a06146bbe013dbb45b22c719e0ffc
sofiarosan <anarodsan@unisabana.edu.co>
2026-03-14 21:55:54 -0500

    random

---
+++ random_q.py
@@ -0,0 +1,9 @@
+import random
+random.seed(123)
+
+start= int(input("Enter the start value:\n"))
+end= int(input("Enter the end value:\n"))
+
+numerox = random.randint (start, end)
+
+print(f"Generated random number: {numerox}")
