aa4b3b1063cb02b5b85117f25bc4f62e17a868a6
sofiarosan <anarodsan@unisabana.edu.co>
2026-03-14 20:56:31 -0500

    function_types 1

---
+++ function_types.py
@@ -0,0 +1,21 @@
+
+#Funcion de promedio
+def calc_avg(i):
+    media= sum(i)/len(i)
+    return media
+datos =[2.0, 4.0, 6.0, 8.0]
+
+prom = calc_avg (datos)
+
+#Funcion de normalizar
+
+def list_shift (datos, prom):
+    for i in range(len(datos)):
+        datos[i]= datos[i]-prom
+
+list_shift (datos, prom)
+
+def print_normalized(datos):
+    print (datos)
+
+print_normalized(datos)
