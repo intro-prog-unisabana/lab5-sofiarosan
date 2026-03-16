43305288cd10f9a3ca3d55f98c6cd46a29933272
sofiarosan <anarodsan@unisabana.edu.co>
2026-03-15 22:40:53 -0500

    calculadora 2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from utils import *
+from utils_calc import *
 
 
 while True:
---
+++ utils_calc.py
@@ -0,0 +1,29 @@
+def add(num1, num2):
+    return num1+num2
+
+def subtract(num1, num2):
+    return num1-num2
+
+def multiply(num1, num2):
+    return num1*num2
+
+def divide(num1, num2):
+    if num2 == 0:
+        return "Error: Division by zero is not allowed."
+    return num1/num2
+
+def exponent(num1, num2):
+    return num1**num2
+
+def modulo(num1, num2):
+    if num2== 0:
+        return "Error: Division by zero is not allowed."
+    return num1%num2
+
+def floor_divide (num1, num2):
+    if num2==0:
+        return "Error: Division by zero is not allowed."
+    return num1 // num2
+
+def absolute(num):
+    return abs(num)
